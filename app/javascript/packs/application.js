b4a1876eeddd6203d52c937cfdf1beea2fb9a64f
yegorch <yegor.rb@gmail.com>
2021-08-10 20:01:18 +0600

    add turbolinks and jquery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,9 +5,18 @@
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
+import "jquery"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+$(function(){
+  console.log('Rails chat');
+});
+
+$(document).on("turbolinks:load", function() {
+  console.log('Document is loaded (turbolinks:load)');
+})
